5627f26f9d2dcff119ca581258dcaba5e0b2f12d
James Alan Bush <jamesalanbush1972@gmail.com>
2023-12-08 07:27:12 -0500

    Initial Commit

---
+++ ChatGPTSwiftUI-MultiturnConversation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatGPTSwiftUI-MultiturnConversation
+//
+//  Created by Xcode Developer on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
