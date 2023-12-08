920907220790ab0a96b24ca45897e313ed000a0a
James Alan Bush <jamesalanbush1972@gmail.com>
2023-12-08 13:56:38 -0500

    Displays the assistant id in the List header

--- ChatGPTSwiftUI-MultiturnConversation/ContentView.swift
+++ ChatGPTSwiftUI-MultiturnConversation/ContentView.swift
@@ -80,7 +80,7 @@ func getAssistant(globalData: GlobalData) {
     request.httpMethod = "POST"
     
     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("Bearer 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", forHTTPHeaderField: "Authorization")
+    request.addValue("Bearer ", forHTTPHeaderField: "Authorization")
     request.addValue("org-jGOqXYFRJHKlnkff8K836fK2", forHTTPHeaderField: "OpenAI-Organization")
     request.addValue("assistants=v1", forHTTPHeaderField: "OpenAI-Beta")
     
