57cf78846bc0f011e1670f64ab9d2af1caae68a1
James Alan Bush <jamesalanbush1972@gmail.com>
2023-12-08 13:55:19 -0500

    Prints the assistant id in the header of the List

--- ChatGPTSwiftUI-MultiturnConversation/ContentView.swift
+++ ChatGPTSwiftUI-MultiturnConversation/ContentView.swift
@@ -14,14 +14,21 @@ struct PlainTextFieldStyle: TextFieldStyle {
     }
 }
 
+class GlobalData: ObservableObject {
+    @Published var someGlobalData: String = "Initial Value"
+}
+
 struct ContentView: View {
+    @ObservedObject var globalData = GlobalData()
+    
     var body: some View {
         VStack {
             // Outer VStack
             VStack {
                 // List VStack
                 List {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+                    TextField("Enter text", text: $globalData.someGlobalData)
+                                .padding()
                     ForEach(1...100, id: \.self) {_ in
                         Section {
                             ForEach(1...2, id: \.self) {_ in
@@ -49,7 +56,7 @@ struct ContentView: View {
                     HStack {
                         Spacer()
                         Button("Button") {
-                            getAssistant()
+                            getAssistant(globalData: globalData)
                         }
                         .padding(.trailing)
                     }
@@ -67,17 +74,16 @@ struct Assistant: Codable {
     let id: String
 }
 
-func getAssistant() {
+func getAssistant(globalData: GlobalData) {
     let url = URL(string: "https://api.openai.com/v1/assistants")!
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
     
     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("Bearer ", forHTTPHeaderField: "Authorization")
+    request.addValue("Bearer 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", forHTTPHeaderField: "Authorization")
     request.addValue("org-jGOqXYFRJHKlnkff8K836fK2", forHTTPHeaderField: "OpenAI-Organization")
     request.addValue("assistants=v1", forHTTPHeaderField: "OpenAI-Beta")
     
-    // Request
     let type: [Dictionary] = [["type": "code_interpreter"]]
     let assistant_request: Dictionary =
     [
@@ -97,6 +103,9 @@ func getAssistant() {
                 if let assistant_response = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] {
                         let id = assistant_response["id"] as? String
                     print("id:\t\(id ?? "")")
+                    DispatchQueue.main.async {
+                        globalData.someGlobalData = id ?? "No ID"
+                    }
                     print(assistant_response)
                 }
             
@@ -108,27 +117,6 @@ func getAssistant() {
     }
     
     task.resume()
-    
-    // Response
-    //    {
-    //      "id": "asst_abc123",
-    //      "object": "assistant",
-    //      "created_at": 1698984975,
-    //      "name": "Math Tutor",
-    //      "description": null,
-    //      "model": "gpt-4",
-    //      "instructions": "You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
-    //      "tools": [
-    //        {
-    //          "type": "code_interpreter"
-    //        }
-    //      ],
-    //      "file_ids": [],
-    //      "metadata": {}
-    //    }
-    
-    
-    
 }
 
 
